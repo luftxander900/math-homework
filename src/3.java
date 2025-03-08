e471a5f09d01e13dcc8a2cabe8ebbd21d8a7571e
luftxander900 <luftxander900@gmail.com>
2025-03-08 07:09:58 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,6 @@
+// This is an example of a simple Java program that prints "Hello World" to the console.
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
