8e4adaef1dd505dcab82ff708651f6fb7d7f0b68
luftxander900 <luftxander900@gmail.com>
2025-03-11 05:05:05 +0000

    Add 6.java

---
+++ src/6.java
@@ -0,0 +1,6 @@
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your name");
+    String name = sc.nextLine();
+    System.out.println("Hello " + name + ", welcome to my math homework.");
+  }
