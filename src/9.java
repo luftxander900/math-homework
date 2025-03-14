f6be7c6a4f098a638a12bebdc8cd32f2154a5e6a
luftxander900 <luftxander900@gmail.com>
2025-03-14 13:40:54 +0000

    Add 9.java

---
+++ src/9.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+  public static void main(String[] args) {
+    double x = 10;
+    double y = 20;
+    double z = 30;
+    System.out.println("The sum of x and y is: " + (x + y));
+    System.out.println("The product of x, y, and z is: " + (x * y * z));
+  }
+}
