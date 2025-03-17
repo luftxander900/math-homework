6f9f059b02a77abd9dcb7037d4e89e1b11fc4f49
luftxander900 <luftxander900@gmail.com>
2025-03-17 06:01:43 +0000

    Add 12.java

---
+++ src/12.java
@@ -0,0 +1,9 @@
+import java.util.*; 
+public class Math {
+	public static void main(String[] args) {
+		Random rnd = new Random();
+		int num1 = rnd.nextInt(10);
+		int num2 = rnd.nextInt(10);
+		System.out.println(num1 + "+"+num2);
+	}
+}
