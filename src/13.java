98a9d3daac924e66ee3035d5487b005a9361d68b
luftxander900 <luftxander900@gmail.com>
2025-03-18 12:49:28 +0000

    Add 13.java

---
+++ src/13.java
@@ -0,0 +1,7 @@
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        double num1 = Math.random() * 50;
+        double num2 = Math.random() * 50;
+        System.out.println("The two numbers are " + num1 + " and " + num2);
+    }
+  
