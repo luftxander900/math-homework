4179543001c171b8d6505209d6fc3359a2c0c38c
luftxander900 <luftxander900@gmail.com>
2025-03-21 07:09:44 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example Java code for math homework
+        int a = 3;
+        int b = 5;
+        System.out.println("The sum of " + a + " and " + b + " is: " + (a + b));
+        System.out.println("The difference between " + a + " and " + b + " is: " + (a - b));
+    }
+}
