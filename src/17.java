b6a3bd26db7ce22dc83c8342a0230141bc925c58
luftxander900 <luftxander900@gmail.com>
2025-03-22 14:54:21 +0000

    Add 17.java

---
+++ src/17.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of using Java to solve a basic arithmetic problem
+        int number1 = 5;
+        int number2 = 3;
+        int result = number1 + number2;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is " + result);
+    }
+}
