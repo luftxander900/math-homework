0e8b32114d87567d6ca5c5e87dc7046a5884b41b
luftxander900 <luftxander900@gmail.com>
2025-03-24 02:13:55 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,23 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example 1: Addition
+        int sum = 5 + 3;
+        System.out.println("The sum of 5 and 3 is: " + sum);
+
+        // Example 2: Subtraction
+        int difference = 8 - 4;
+        System.out.println("The difference between 8 and 4 is: " + difference);
+
+        // Example 3: Multiplication
+        int product = 7 * 6;
+        System.out.println("The product of 7 and 6 is: " + product);
+
+        // Example 4: Division
+        double division = 10 / 2;
+        System.out.println("The result of 10 divided by 2 is: " + division);
+
+        // Example 5: Exponentiation (Power)
+        int exponentiation = 2 ^ 3;
+        System.out.println("The result of 2 raised to the power of 3 is: " + exponentiation);
+    }
+}
