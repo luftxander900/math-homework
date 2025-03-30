c84b966e0fe9b7e3ff9a226135a5eb06fd4e5675
luftxander900 <luftxander900@gmail.com>
2025-03-30 20:05:44 +0000

    Add 21.java

---
+++ src/21.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example calculation: 5 + 3 = 8
+        int result = 5 + 3;
+        System.out.println("The sum is: " + result);
+    }
+}
