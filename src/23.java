123d3c5f606a51e05cbc0480d526ae4bccadf4ff
luftxander900 <luftxander900@gmail.com>
2025-04-01 14:32:44 +0000

    Add 23.java

---
+++ src/23.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 3.141592653589793238;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        // Here you can add more code to solve the math problem
+    }
+}
