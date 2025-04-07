1e108451fcda4a99a0782845233e19d41028338f
luftxander900 <luftxander900@gmail.com>
2025-04-07 20:05:09 +0000

    Add 28.java

---
+++ src/28.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code for an arithmetic operation
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("The result of " + num1 + " + " + num2 + " is: " + (num1 + num2));
+    }
+}
