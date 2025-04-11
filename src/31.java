2aa8df9f1d73025ec9c6fc6c9499f74472b9ffbd
luftxander900 <luftxander900@gmail.com>
2025-04-11 11:47:04 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of mathematical computation and output in Java
+        System.out.println("Adding 5 to 10 gives: " + (5 + 10));
+        
+        // Another example of using loops with numbers
+        for(int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
