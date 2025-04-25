cf7ed87a5c9e9bc53ec94f4010261a51824904c2
luftxander900 <luftxander900@gmail.com>
2025-04-25 12:23:03 +0000

    Add 34.java

---
+++ src/34.java
@@ -0,0 +1,13 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of simple arithmetic operations in Java
+        int result1 = 5 + 3;
+        System.out.println("The sum is: " + result1);
+
+        double result2 = 7.5 - 2.0;
+        System.out.println("The difference is: " + result2);
+
+        String result3 = "Hello, World!";
+        System.out.println(result3);
+    }
+}
