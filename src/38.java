7788ee1018be9d9fcd7a1a4ba324163dbd67a3e3
luftxander900 <luftxander900@gmail.com>
2025-05-18 08:40:24 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example: Addition of two numbers using simple assignment operator
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
