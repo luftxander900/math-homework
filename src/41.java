9d06efc79f239e8cfec2c5ae00fe770b5ca652ee
luftxander900 <luftxander900@gmail.com>
2025-05-24 21:09:56 +0000

    Add 41.java

---
+++ src/41.java
@@ -0,0 +1,12 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+
+        double sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        int product = num1 * num2;
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
