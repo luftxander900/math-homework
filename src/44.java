a9e1c798d56f3cc58f64cafef7b3d8ba1c2efc3e
luftxander900 <luftxander900@gmail.com>
2025-05-27 12:13:16 +0000

    Add 44.java

---
+++ src/44.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of a simple arithmetic expression: 5 + 3 * 2 - 10 / 4
+        double result = (5 + 3 * 2) - (10 / 4);
+        
+        System.out.println("The result is: " + result);
+    }
+}
